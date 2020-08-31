0f014d91bfe55b7cfcd90e5ba75a8625f4b8efb3
rkrdovrgs <rkrdo.vrgs@gmail.com>
2020-08-31 01:18:29 -0400

    setting pin to current location

--- src/routes/Home/components/Home.js
+++ src/routes/Home/components/Home.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import {View, Text} from 'react-native';
 import MapContainer from './MapContainer';
 import {Container} from 'native-base';
 
 export default class Home extends React.Component {
   componentDidMount() {
-    this.props.setName();
+    this.props.getCurrentLocation();
   }
 
   render() {
-    const region = {
-      latitude: 26.124161,
-      longitude: -80.143593,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    };
     return (
       <Container>
-        <MapContainer region={region}></MapContainer>
+        {this.props.region.latitude ? (
+          <MapContainer region={this.props.region}></MapContainer>
+        ) : (
+          <></>
+        )}
       </Container>
     );
   }
